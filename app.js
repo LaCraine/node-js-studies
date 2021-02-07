10bec90db649df9f6c7fcc7709be45afc2017d24
LaCraine <mateuszsyma95@gmail.com>
2021-02-07 11:16:57 +0100

    push to world

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { join } = require("path");
 const path = require("path");
 const fromAnotherFile = require("./functions");
 
-const port = 3000;
+const port = process.env.PORT || 3000; //pod kątem hostingu
 const app = express();
 
 
