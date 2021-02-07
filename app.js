5be7b5df2f5be4bd4fa753bcde99cce0fa9b0601
LaCraine <mateuszsyma95@gmail.com>
2021-02-07 09:14:07 +0100

    server

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const functions = require("./functions");
+const http = require("http");
+
+//handler na wyświetlanie
+
+const handler = (request, response) => {
+    console.log("sample message")
+}
 
 
-console.log("hello");
+const server = http.createServer(handler);
+const port = 3000;
 
-functions.helloWorld();
-functions.add(4,5);
+
+
+
+server.listen(port, () => {
+    console.log(`Serwer działa na porcie ${port}`)
+});
